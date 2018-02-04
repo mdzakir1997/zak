b491b36d3428bb73539f2ee2566fad4aa7724ddc
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-02-04 09:12:21 +0530

    Rename fact.c to fact1.c

---
+++ fact1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,i,fact=1;
+printf("\n Enter a number");
+scanf("%d",a);
+for(i=a;i>o;i--)
+{
+fact*=i;
+}
+printf("\n The factorial of a num is %d",fact);
+}
