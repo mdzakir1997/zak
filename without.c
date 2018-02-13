a9c8a5e64b352701e8ee22bf1dbdc7aaf7be6258
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-02-13 16:38:08 +0530

    Update without.c

--- without.c
+++ without.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<conio.h>
 #include<string.h>
 void main()
 {
     int a=0,i,len;
     char z[50];
+    clrscr();
     printf("\n Enter the string");
  gets(z);
     len = strlen(z);
@@ -13,4 +15,5 @@ void main()
      a++;   
     }
 printf("\nThe number of words is %d",a+1);
+getch();
 }
