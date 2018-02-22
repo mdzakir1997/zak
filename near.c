cd3226d3ac35852694b73198c835fe6b87b011ab
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-02-22 15:47:32 +0530

    Create near.c

---
+++ near.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    clrscr();
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    for(i=a;i>0;i--)
+    {
+        if(i%2==0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
