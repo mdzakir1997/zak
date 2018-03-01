552ff4e7c937f02f8ee59eefeb08015aa25f1ae1
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-03-01 16:00:10 +0530

    Create strsk.c

---
+++ strsk.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char s[20];
+int i,k;
+printf("Enter the string\n");
+gets(s);
+printf("\nEnter the number");
+scanf("%d",&k);
+for(i=o;i<k;i++)
+{
+printf("%c",s[i]);
+}
+}
